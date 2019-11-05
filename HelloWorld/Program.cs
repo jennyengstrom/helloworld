c50526ff79a9df63cbf77354347dd92a88db9304
Jenny Engstrom <engstromjenny@yahoo.com>
2019-11-05 11:34:01 +0100

    a frustrated user. i can not get it to work

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Skriv ett tal med decimaler här:");
+            string read = Console.ReadLine();
+            double decimalNbr = double.Parse(read);
+
+            Console.WriteLine("Hur många decimaler vill du ha?:");
+            read = Console.ReadLine();
+            int paces = int.Parse(read);
+
+            var roundedNbr = Math.Round(decimalNbr, paces);
+            Console.WriteLine($"Avrundat: {roundedNbr}");
+
+            Console.Read();
+        }
+    }
+}
